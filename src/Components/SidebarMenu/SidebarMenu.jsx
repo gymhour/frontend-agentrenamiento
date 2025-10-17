153df6c9db6abf461c2235e21bcee39f749bc1ef
Pedro <pedrocarvi@outlook.com>
2025-10-17 19:08:40 -0300

    Vuelvo a habilitar admin rutinas asignadas

--- src/Components/SidebarMenu/SidebarMenu.jsx
+++ src/Components/SidebarMenu/SidebarMenu.jsx
@@ -211,7 +211,7 @@ const SidebarMenu = ({ isAdmin, isEntrenador }) => {
                       <AsignarRutinaIcon className="icon" color="#A2A2A2" /> Asignar Rutinas
                     </li>
                   </Link>
-                  {/* <Link
+                  <Link
                       to="/admin/rutinas-asignadas"
                       className={`menu-link ${location.pathname === "/admin/rutinas-asignadas" ? "active" : ""
                         }`}
@@ -219,7 +219,7 @@ const SidebarMenu = ({ isAdmin, isEntrenador }) => {
                       <li className="menu-item">
                         <AsignarRutinaIcon className="icon" color="#A2A2A2" /> Rutinas Asignadas
                       </li>
-                  </Link> */}
+                  </Link>
                   <Link
                     to="/admin/rutinas"
                     className={`menu-link ${location.pathname === "/admin/rutinas"
